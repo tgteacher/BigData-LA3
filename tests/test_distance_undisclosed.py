73f8e7f4b587edbe97acf62bb3cfbbc6bd0e6e09
Tristan Glatard <tristan.glatard@concordia.ca>
2018-04-30 12:03:51 -0400

    Fixed test distance undisclosed

--- tests/test_distance_undisclosed.py
+++ tests/test_distance_undisclosed.py
@@ -5,7 +5,7 @@ def test_distance():
     process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     code=process.wait()
     assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")=="1708"+os.linesep)
+    assert(process.stdout.read().decode("utf-8")=="12284"+os.linesep)
 
     command="python ./answers/distance2.py ./data/plants.data on vt"
     process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
