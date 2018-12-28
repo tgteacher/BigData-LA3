f6d9649c5f4b4685284c615a427733a85af39f69
Martin <martin.khannouz@gmail.com>
2018-12-28 06:59:49 -0400

    Update association rules exercices.

--- tests/test_association_rules.py
+++ tests/test_association_rules.py
@@ -1,10 +1,7 @@
-import subprocess, os
+import sys
+sys.path.insert(0, './answers')
+from answer import association_rules
 
 def test_association_rules():
-    command="python ./answers/association_rules.py ./data/plants.data 15 0.1 0.3"
-    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    code=process.wait()
-    assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")==open("tests/association_rules.txt","r").read())
-
-
+    a = association_rules("./data/plants.data", 15, 0.1, 0.3)
+    assert(a==open("tests/association_rules.txt","r").read())
--- tests/test_association_rules_undisclosed.py
+++ tests/test_association_rules_undisclosed.py
@@ -1,10 +1,7 @@
-import subprocess, os
+import sys
+sys.path.insert(0, './answers')
+from answer import association_rules
 
 def test_association_rules():
-    command="python ./answers/association_rules.py ./data/plants.data 17 0.2 0.4"
-    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    code=process.wait()
-    assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")==open("tests/association_rules_undisclosed.txt","r").read())
-
-
+    a = association_rules("./data/plants.data", 17, 0.2, 0.4)
+    assert(a==open("tests/association_rules_undisclosed.txt","r").read())
