3ea13f46804f68a9c9196a689f0d0c02bb5d2818
Martin <martin.khannouz@gmail.com>
2018-12-27 23:13:20 -0400

    First add.
    Edit tests from data_frame as well as the result wich is in a CSV file.

--- tests/test_data_frame.py
+++ tests/test_data_frame.py
@@ -1,8 +1,7 @@
-import subprocess, os
+import sys
+sys.path.insert(0, './answers')
+from answer import data_frame
 
 def test_data_frame():
-    command="python ./answers/data_frame.py ./data/plants.data 11"
-    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    code=process.wait()
-    assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")==open("tests/data_frame.txt","r").read())
+    a = data_frame("./data/plants.data", 11)
+    assert(a==open("tests/data_frame.txt","r").read())
--- tests/test_data_frame_undisclosed.py
+++ tests/test_data_frame_undisclosed.py
@@ -1,8 +1,7 @@
-import subprocess, os
+import sys
+sys.path.insert(0, './answers')
+from answer import data_frame
 
 def test_data_frame_undisclosed():
-    command="python ./answers/data_frame.py ./data/plants.data 14"
-    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    code=process.wait()
-    assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")==open("tests/data_frame_undisclosed.txt","r").read())
+    a = data_frame("./data/plants.data", 14)
+    assert(a==open("tests/data_frame_undisclosed.txt","r").read())
