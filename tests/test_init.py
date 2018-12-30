590bdac7ad5ada61d0cebd4999abbf46ef82a975
Martin <martin.khannouz@gmail.com>
2018-12-30 15:29:22 -0400

    Update test for init_centroid.

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,8 +1,8 @@
-import subprocess, os
+import sys
+sys.path.insert(0, './answers')
+from answer import init_centroids
 
-def test_init():
-    command="python ./answers/init.py 3 124"
-    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    code=process.wait()
-    assert(not code), "Command failed"
-    assert(process.stdout.read().decode("utf-8")==open("tests/init.txt","r").read())
+def test_init_centroids():
+    res = ["oh", "ab", "mi"]
+    a = init_centroids(3, 124)
+    assert(a == res)
